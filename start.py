b8993e7f5a1f269f7ba144c09853ae05da7fa0a8
FlorianMiceli <florian.miceli@universite-paris-saclay.fr>
2023-02-14 17:32:23 +0100

    token

--- start.py
+++ start.py
@@ -41,7 +41,8 @@ def getProductsAndMenus(nearest_bk_id):
 # crowns 
 def getCrownProducts():
     URL = 'https://webapi.burgerking.fr/blossom/api/v12/kingdom/points'
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36','authorization' :'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3NTUxMjQzMTQzODcwODAxMTc1IiwiaWF0IjoxNjc2MTE1MTc1LCJpc3MiOiJCdXJnZXJLaW5nIEZyYW5jZSIsInN0YXkiOnRydWUsImV4cCI6MTY3ODcwNzQwMX0.frMnOfG9fWbgFRV9V-wNgCIApINUVpcs1z8tHaL9H_Y'}
+    AUTH_TOKEN = ''
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36','authorization' : AUTH_TOKEN}
     r = requests.get(URL, headers=headers)
     data = r.json()
     # save json
